132652ef0c636e1872fefe7b152192108a1d535e
ACodeDuniya <147372637+ACodeDuniya@users.noreply.github.com>
2023-10-12 19:39:21 +0530

    Add files via upload

---
+++ 06.js
@@ -0,0 +1,8 @@
+
+// Select the 2nd list item and give it a green background color
+var secondListItem = document.querySelector('#items li:nth-child(2)');
+secondListItem.style.backgroundColor = 'green';
+
+// Select the 3rd list item and make it invisible
+var thirdListItem = document.querySelector('#items li:nth-child(3)');
+thirdListItem.style.display = 'none';
